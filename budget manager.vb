595c5ae586b6c195a31a116556a1f7b318dc241b
Jake Callcut <jakecallcut123@gmail.com>
2019-12-21 15:37:40 +0000

    fixed budget manager bugs

--- budget manager.vb
+++ budget manager.vb
@@ -2,14 +2,14 @@
 
     Dim items(999) As String
     Dim prices(999) As Double
+    Dim totalexpense As Double
+    Dim remaining As Double
 
     Private Sub btnadd_Click(sender As Object, e As EventArgs) Handles btnadd.Click
 
         Dim item As String = ""
         Dim price As Double
         Dim budget As Double
-        Dim totalexpense As Double
-        Dim remaining As Double
 
         budget = txtbudget.Text
         item = InputBox("Please enter the name of the item or expense")
@@ -32,7 +32,6 @@
             MessageBox.Show("Please enter a number in the budget field")
         End If
 
-
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnback.Click
@@ -45,16 +44,20 @@
     Private Sub btnclr_Click(sender As Object, e As EventArgs) Handles btnclr.Click
 
         lstbxbudget.Items.Clear()
+        lblremaining.Text = "Remaining:"
+        lbltotal.Text = "Total Expenses:"
+        totalexpense = 0
+        remaining = 0
 
     End Sub
 
     Private Sub btnsrt_Click(sender As Object, e As EventArgs) Handles btnsrt.Click
 
-        Dim counter As Integer
+        'Dim counter As Integer
 
-        For counter = 0 To lstbxbudget.Items
+        'For counter = 0 To lstbxbudget.Items
 
-        Next
+        'Next
 
     End Sub
 End Class
